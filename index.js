62c4f9bf409bc6c95eb97ad46bb0a319fb571bf5
Your Name <you@example.com>
2024-03-10 20:50:01 +0800

    a

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow } = require('electron');
 const { SerialPort } = require('serialport');
+const { ipcMain } = require('electron');
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -9,7 +10,11 @@ function createWindow() {
       nodeIntegration: true
     }
   });
+
+
   win.loadFile('index.html');
+
+
   const port = new SerialPort({ path: 'COM3', baudRate: 9600 });
   port.write('Hello MCU');
 
@@ -18,4 +23,15 @@ function createWindow() {
   });
 }
 
+
 app.whenReady().then(createWindow);
+
+// 监听来自 Vue 组件的数据请求
+ipcMain.on('sendData', (event, data) => {
+  console.log('Received data from Vue component:', data);
+  
+  // 在这里处理数据，您可以执行任何您需要的逻辑
+
+  // 发送响应数据到 Vue 组件
+  event.reply('responseData', 'Data received successfully!');
+});
